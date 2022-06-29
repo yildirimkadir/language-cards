08c7d46389bca535cfe3c84cc27adf4c1d37e849
yildirimkadir <yildirimkad34@gmail.com>
2022-06-29 23:40:52 +0200

    Das Projekt wurde erstellt.

---
+++ src/components/item/Item.js
@@ -0,0 +1,13 @@
+import './Item.css';
+const Item = ({ card }) => {
+  const { name, img, options } = card;
+
+  return (
+    <div className="card">
+      <img className="card-logo" src={img} alt="" />
+      <h3 className="card-title">{name}</h3>
+    </div>
+  );
+};
+
+export default Item;
